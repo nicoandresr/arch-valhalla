6d8493066fcbc59535d457bdbf518d492cf0ad6c
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-12-18 22:15:49 -0500

    Remove console logs

--- routes.ts
+++ routes.ts
@@ -20,9 +20,6 @@ export class Routes {
         let url = "https://gist.githubusercontent.com/nicoandresr/7171f107b6d3bfa0d23ccf514dd3869d/raw/8430b4667e34c52267d380708ea6ab461dab8dfa/.config.fish";
         let options = Routes.getRequestOptions(url);
         request(options, (err, response, body) => {
-            console.log(body);
-            console.log(response);
-            console.log(err);
             if (err || response.statusCode !== 200) {
                 res.status(response.statusCode).send(response.statusMessage);
                 return;
