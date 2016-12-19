936c976864dfc4dca1aa5b97d09545fc508be0fa
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-12-18 22:06:57 -0500

    Config route for .config.fish

--- routes.ts
+++ routes.ts
@@ -1,12 +1,37 @@
 import * as express from "express";
+import * as request from "request";
 
 export class Routes {
 
-    constructor() {
+    public config(): express.Router {
+        
         let router = express.Router();
 
-        router.get("/", (req, res) => res.send("hola desde router"));
+        this.configFish(router);
 
         return router;
+    }
+
+    private configFish(router: express.Router): void {
+        router.get("/get/config-fish/", this.configFishHandler);
+    }
+
+    private configFishHandler(req, res): void {
+        let url = "https://gist.githubusercontent.com/nicoandresr/7171f107b6d3bfa0d23ccf514dd3869d/raw/8430b4667e34c52267d380708ea6ab461dab8dfa/.config.fish";
+        let options = this.getRequestOptions(url);
+        request(options, (err, response, body) => {
+            if (err || response.statusCode !== 200) {
+                res.status(response.statusCode).send(response.statusMessage);
+                return;
+            }
+
+            res.send(JSON.parse(body));
+        });
+    }
+
+    private getRequestOptions(url: string) {
+        return {
+            url: url
+        };
     }
 }
--- server.ts
+++ server.ts
@@ -6,23 +6,28 @@ class Server {
 
     private _app: express.Application;
 
-    constructor(port: number){
+    constructor(private _port: number){
         this.setupServer();
         this.enableCors();
-
-        this._app.listen(port, () => console.log("Listen on port: "+ port));
+        this.runServer();
     }
 
     private setupServer(): void {
         this._app = express();
 
-        let routes = new Routes();
+        let routes = new Routes().config();
         this._app.use("/", routes); 
     }
 
     private enableCors(): void {
        let options = { origin: "*" };
        this._app.use(cors(options));
+    }
+
+    private runServer(): void {
+        this._app.listen(this._port, () => {
+            console.log("Listen on port: " + this._port);
+        });
     }
 }
 
